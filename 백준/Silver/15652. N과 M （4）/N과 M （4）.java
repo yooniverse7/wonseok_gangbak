97378be0b24a255b25543303066878e3ef428caf
CheonSse <128568993+yooniverse7@users.noreply.github.com>
2024-08-27 17:02:13 +0900

    [Silver III] Title: N과 M (4), Time: 96 ms, Memory: 20292 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static int[] arr;
+	public static boolean[] visited;
+	public static int N, M;
+	public static StringBuilder sb;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[M];
+		visited = new boolean[N];
+		dfs(0, 1);		
+		System.out.println(sb.toString());
+	}
+
+	public static void dfs(int depth, int idx) { // 깊이랑 현재 깊이를 저장하는 매개변수(끝 값)
+		if(depth == M) {
+			for(int i = 0; i<M; i++) {
+				sb.append(arr[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=idx; i<=N; i++) {
+			arr[depth] = i;
+			dfs(depth + 1, i);
+		}
+	}
+
+}
