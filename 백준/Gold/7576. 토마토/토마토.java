b7d2ced66978b8e70a7b38d432d5dfe3b5bad22a
CheonSse <128568993+yooniverse7@users.noreply.github.com>
2024-08-29 00:22:36 +0900

    [Gold V] Title: 토마토, Time: 524 ms, Memory: 102204 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int N, M;
+    public static int[][] tomato;
+    public static boolean[][] visited;
+    public static int[] dx = {-1, 1, 0, 0};
+    public static int[] dy = {0, 0, -1, 1};
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        tomato = new int[M][N];
+        visited = new boolean[M][N];
+        
+        Deque<int[]> dq = new ArrayDeque<>();
+        
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                tomato[i][j] = Integer.parseInt(st.nextToken());
+                if (tomato[i][j] == 1) {
+                    dq.offer(new int[] {i, j}); // 익은 토마토를 모두 큐에 넣음
+                    visited[i][j] = true;
+                } else if (tomato[i][j] == -1) {
+                    visited[i][j] = true; // 토마토가 없는 곳도 방문 처리
+                }
+            }
+        }
+        
+        bfs(dq);
+        
+        int max = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tomato[i][j] == 0) { // 익지 않은 토마토가 있다면
+                    System.out.println("-1");
+                    return;
+                }
+                max = Math.max(max, tomato[i][j]);
+            }
+        }
+        
+        System.out.println(max - 1); // 첫 날을 1로 시작했으므로 최종 값에서 1을 뺌
+    }
+    
+    public static void bfs(Deque<int[]> dq) {
+        while (!dq.isEmpty()) {
+            int[] n = dq.poll();
+            int nx = n[0];
+            int ny = n[1];
+            for (int i = 0; i < 4; i++) {
+                int nextX = nx + dx[i];
+                int nextY = ny + dy[i];
+                if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N || visited[nextX][nextY]) 
+                    continue;
+                
+                if (tomato[nextX][nextY] == 0) { // 안 익은 토마토를 발견하면
+                    tomato[nextX][nextY] = tomato[nx][ny] + 1; // 익히고, 날짜 증가
+                    visited[nextX][nextY] = true;
+                    dq.offer(new int[] {nextX, nextY});
+                }
+            }
+        }
+    }
+}
