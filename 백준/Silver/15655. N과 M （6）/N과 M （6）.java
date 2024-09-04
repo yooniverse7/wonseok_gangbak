57b67c1dc5d2f30ec4a691db34262be393721d91
CheonSse <128568993+yooniverse7@users.noreply.github.com>
2024-09-04 11:48:36 +0900

    [Silver III] Title: N과 M (6), Time: 64 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -4,49 +4,49 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+	public static StringBuilder sb = new StringBuilder();
+	public static int N,M;
 	public static int[] arr;
-	public static int[] arr1;
 	public static boolean[] visited;
-	public static int N, M;
-	public static StringBuilder sb;
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr1 = new int[N];
-		StringTokenizer str = new StringTokenizer(br.readLine());
-		for(int i = 0; i<N; i++) {
-			arr1[i] = Integer.parseInt(str.nextToken());
-		}
-		Arrays.sort(arr1); // 정렬하기
-		
-		arr = new int[M];
-		visited = new boolean[N];
-		dfs(0, 0);		
-		System.out.println(sb.toString());
+	public static int[] arr1;
+	
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	arr = new int[M];
+    	visited = new boolean[N];
+    	arr1 = new int[N];
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for(int i = 0; i<N; i++) {
+    		arr1[i] = Integer.parseInt(st.nextToken());
+    	}
+    	Arrays.sort(arr1);
+    	
+    	dfs(0, 0);
+    	System.out.println(sb.toString());
 	}
-
-	public static void dfs(int depth, int idx) { // 깊이랑 현재 깊이를 저장하는 매개변수(끝 값)
-		if(depth == M) {
+    
+    public static void dfs(int depth, int idx) {
+    	if (depth == M) {
 			for(int i = 0; i<M; i++) {
 				sb.append(arr[i] + " ");
 			}
 			sb.append("\n");
 			return;
 		}
-		for(int i=idx; i<N; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				arr[depth] = arr1[i];
-				dfs(depth + 1, i);
-				visited[i] = false;
-			}
-			
-		}
+    	
+    	for(int i = idx; i<N; i++) {
+    		if(!visited[i]) {
+    			visited[i] = true;
+    			arr[depth] = arr1[i];
+    			dfs(depth+1, i);
+    			visited[i] = false;
+    		}
+    	}
 	}
 
 }
