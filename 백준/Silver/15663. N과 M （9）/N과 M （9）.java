973f000fe54495d2e73ede6a7a1fa9d7ae22bb41
CheonSse <128568993+yooniverse7@users.noreply.github.com>
2024-09-04 12:19:42 +0900

    [Silver II] Title: N과 M (9), Time: 156 ms, Memory: 35396 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,65 +1,53 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-
 public class Main {
-	public static int N, M;
-	public static int[] result;
-	public static boolean[] visited;
+	public static StringBuilder sb = new StringBuilder();
+	public static int N,M;
 	public static int[] arr;
-	public static HashSet<String> hs;
-	public static StringBuilder sb, sb2;
+	public static int[] arr1;
+	public static boolean[] visited;
 	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		sb = new StringBuilder();
-		visited = new boolean[N];
-		arr = new int[N];
-		result = new int[M];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i =0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		
-		dfs(0);
-		
-		System.out.println(sb.toString());
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	arr = new int[M];
+    	arr1 = new int[N];
+    	visited = new boolean[N];
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for(int i = 0; i<N; i++) {
+    		arr1[i] = Integer.parseInt(st.nextToken());
+    	}
+    	Arrays.sort(arr1);
+    	
+    	dfs(0);
+    	System.out.println(sb.toString());
 	}
-	
-	public static void dfs(int depth) {
-		HashSet<Integer> hs = new HashSet<>();
-		if (depth == M) {
+    
+    public static void dfs(int depth) {
+    	if (depth == M) {
 			for(int i = 0; i<M; i++) {
-				sb.append(result[i] + " ");
+				sb.append(arr[i] + " ");
 			}
 			sb.append("\n");
 			return;
 		}
-		
-		for (int i = 0; i<N; i++) {
-			if (!visited[i] && !hs.contains(arr[i])) {
-				visited[i] = true;
-				result[depth] = arr[i];
-				hs.add(result[depth]);
-				dfs(depth + 1);
-				visited[i] = false;
-			}
-		}
-		
+    	int temp = -1;
+    	for(int i = 0; i<N; i++) {
+    		if(!visited[i] && temp != arr1[i]) {
+    			temp = arr1[i];
+    			visited[i] = true;
+    			arr[depth] = arr1[i];
+        		dfs(depth+1);
+        		visited[i] = false;
+    		}
+    	}
 	}
 
 }
