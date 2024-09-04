2295ccc6c7721ad58704e0349c0dbf85418bb981
CheonSse <128568993+yooniverse7@users.noreply.github.com>
2024-09-04 10:48:59 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,42 +4,43 @@ import java.util.StringTokenizer;
 
 public class Main {
 	
-	public static int[] arr;
-	public static boolean[] visited;
-	public static int N, M;
 	public static StringBuilder sb;
+	public static int[] arr;
+	public static int N, M;
+	public static boolean[] visited;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		arr = new int[M];
-		visited = new boolean[N];
-		dfs(0, 0);		
-		System.out.println(sb.toString());
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st;
+    	sb = new StringBuilder();
+    	st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	arr = new int[M];
+    	visited = new boolean[N];
+    	
+    	dfs(0, 1);
+    	System.out.println(sb.toString());
 	}
-
-	public static void dfs(int depth, int idx) { // 깊이랑 현재 깊이를 저장하는 매개변수(끝 값)
-		if(depth == M) {
-			for(int i = 0; i<M; i++) {
+    
+    public static void dfs(int depth , int idx) {
+    	
+    	if (depth == M) {
+			for (int i = 0; i < M; i++) {
 				sb.append(arr[i] + " ");
 			}
 			sb.append("\n");
 			return;
 		}
-		for(int i=idx+1; i<=N; i++) { 
-			if(!visited[i-1]) {
-				visited[i-1] = true;
-				arr[depth] = i;
-				dfs(depth + 1, i);
-				visited[i-1] = false;
-			}
-			
-			
-		}
+		
+    	for(int i = idx; i<=N; i++) {
+    		if(!visited[i-1]) {
+    			visited[i-1] = true;
+    			arr[depth] = i;
+    			dfs(depth+1, i);
+    			visited[i-1] = false;
+    		}
+    	}
 	}
 
 }
