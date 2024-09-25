07a378423c626fe6e263a4bade1742682e808127
CheonSse <128568993+yooniverse7@users.noreply.github.com>
2024-09-25 23:22:05 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+	
+		int N = Integer.parseInt(br.readLine());
+		long[] arr = new long[N+1];
+		
+		arr[0] = 0;
+		arr[1] = 1;
+		
+		for(int i = 2; i<=N; i++) {
+			arr[i] = arr[i-2] + arr[i-1];
+		}
+		
+		System.out.println(arr[N]);
+	}
+		
+}
