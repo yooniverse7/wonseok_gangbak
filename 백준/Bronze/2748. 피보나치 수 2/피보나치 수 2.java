6b3344c234bf2112d3e4bdb8965c4cd859fdcdad
CheonSse <128568993+yooniverse7@users.noreply.github.com>
2024-10-15 21:04:43 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -1,33 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	// 미리 90으로 배열을 만들어두고 시작
+	// 0번째와 1번째는 같으니 담아두고 배열 계속 채우기
+	// 사이즈가 커지니 long으로 담기
 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-	
-		int N = Integer.parseInt(br.readLine());
-		long[] arr = new long[N+1];
-		
-		arr[0] = 0;
-		arr[1] = 1;
-		
-		for(int i = 2; i<=N; i++) {
-			arr[i] = arr[i-2] + arr[i-1];
-		}
-		
-		System.out.println(arr[N]);
-	}
-		
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        long[] arr = new long[91];
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        
+        for(int i = 2; i<91; i++) {
+        	arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+        System.out.println(arr[Integer.parseInt(br.readLine())]);
+        
+     }
+
 }
